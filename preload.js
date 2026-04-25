15d86fdca4c7b08e42c169a7a1d4e76db62b262b
cypress_sticker <tky1192tky@gmail.com>
2026-04-25 11:02:59 +0900

    feat: add preload.js with context bridge

---
+++ preload.js
@@ -0,0 +1,39 @@
+// preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+const IPC = require('./src/utils/ipc-channels');
+
+contextBridge.exposeInMainWorld('api', {
+  // Auth
+  startLogin: () => ipcRenderer.invoke(IPC.AUTH_START_LOGIN),
+  logout: () => ipcRenderer.send(IPC.AUTH_LOGOUT),
+  onLoginSuccess: (cb) => ipcRenderer.on(IPC.AUTH_LOGIN_SUCCESS, (e, data) => cb(data)),
+  onLoginError: (cb) => ipcRenderer.on(IPC.AUTH_LOGIN_ERROR, (e, msg) => cb(msg)),
+  onAuthStatus: (cb) => ipcRenderer.on(IPC.AUTH_STATUS, (e, data) => cb(data)),
+
+  // Settings
+  loadSettings: () => ipcRenderer.invoke(IPC.SETTINGS_LOAD),
+  saveSettings: (settings) => ipcRenderer.invoke(IPC.SETTINGS_SAVE, settings),
+  exportSettings: () => ipcRenderer.invoke(IPC.SETTINGS_EXPORT),
+  importSettings: () => ipcRenderer.invoke(IPC.SETTINGS_IMPORT),
+  resetSettings: () => ipcRenderer.invoke(IPC.SETTINGS_RESET),
+
+  // Alert Server
+  startAlertServer: () => ipcRenderer.send(IPC.ALERT_START),
+  stopAlertServer: () => ipcRenderer.send(IPC.ALERT_STOP),
+  onAlertEvent: (cb) => ipcRenderer.on(IPC.ALERT_EVENT, (e, data) => cb(data)),
+  onAlertStatus: (cb) => ipcRenderer.on(IPC.ALERT_STATUS, (e, data) => cb(data)),
+
+  // Bot Server
+  startBotServer: () => ipcRenderer.send(IPC.BOT_START),
+  stopBotServer: () => ipcRenderer.send(IPC.BOT_STOP),
+  onBotStatus: (cb) => ipcRenderer.on(IPC.BOT_STATUS, (e, data) => cb(data)),
+
+  // Overlay
+  getOverlayUrl: () => ipcRenderer.invoke(IPC.OVERLAY_URL),
+  onShowAlert: (cb) => ipcRenderer.on(IPC.OVERLAY_SHOW_ALERT, (e, data) => cb(data)),
+
+  // Cleanup
+  removeAllListeners: () => {
+    Object.values(IPC).forEach(channel => ipcRenderer.removeAllListeners(channel));
+  },
+});
